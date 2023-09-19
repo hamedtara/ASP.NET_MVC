4b3ab9190dc9a31bf105f642fd4d94f2441c2286
Hamed <122459658+hamedtara@users.noreply.github.com>
2023-09-19 10:23:28 -0400

    Loan Calculation: An ASP.NET MVC Application
    I have crafted a sleek ASP.NET MVC application designed for effortless loan calculation.

---
+++ Loan Calculation/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                decimal futureValue = model.CalculateFutureValue();
+                ViewBag.FV = futureValue;
+            }
+            else
+            {
+                ViewBag.Fv = 0;
+            }
+
+            return View(model);
+        }
+    }
+}
